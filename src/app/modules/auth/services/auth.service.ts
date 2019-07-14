238c3792e9c60d07a8cebfc077485450b7cafb20
Paulo Santos <tm.paulo.santos@gmail.com>
2019-07-14 11:23:18 +0100

    Improved auth service.

--- src/app/modules/auth/services/auth.service.ts
+++ src/app/modules/auth/services/auth.service.ts
@@ -1,5 +1,10 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Credential } from '../models/credential';
+import { Observable } from 'rxjs';
+import { environment as env } from '@env/environment';
+import { Router } from '@angular/router';
+import * as moment from 'moment';
 
 @Injectable({
   providedIn: 'root'
@@ -7,6 +12,33 @@ import { HttpClient } from '@angular/common/http';
 export class AuthService {
 
   constructor(
-    private readonly http: HttpClient
+    private readonly http: HttpClient,
+    private readonly router: Router
   ) {}
+
+  signin(data: Credential): Observable<any> {
+    return this.http.post<Credential>(this._makeUrl('signup'), data);
+  }
+
+  signup(data: Credential) {
+    return this.http.post<Credential>(this._makeUrl('signin'), data);
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwt_token');
+    localStorage.removeItem('jwt_expires_in');
+    this.router.navigateByUrl('/signin');
+  }
+
+  isLoggedIn(): boolean {
+    return moment().isBefore(this.expiresin);
+  }
+
+  private _makeUrl(uri: string = ''): string {
+    return `${env.api_url}/${uri}`;
+  }
+
+  get expiresin(): string|null {
+    return localStorage.getItem('jwt_expires_in');
+  }
 }
