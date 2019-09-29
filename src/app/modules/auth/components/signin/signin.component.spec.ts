2dc6174b6fda2634c51a6bb2e89244cfdc2da144
Paulo Santos <tm.paulo.santos@gmail.com>
2019-09-29 11:47:13 +0100

    Added some incomplete tests.

--- src/app/modules/auth/components/signin/signin.component.spec.ts
+++ src/app/modules/auth/components/signin/signin.component.spec.ts
@@ -1,57 +1,112 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { async, ComponentFixture, TestBed, tick, inject, fakeAsync } from '@angular/core/testing';
 
 import { HttpClientModule } from '@angular/common/http';
 import { SigninComponent } from './signin.component';
 import { SharedModule } from '@app/modules/shared/shared.module';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, Router } from '@angular/router';
 import { Component } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { Location } from '@angular/common';
+import { RouterTestingModule } from '@angular/router/testing';
 
 @Component({
   selector: '',
   template: ''
 })
-class LoginComponent {}
+class SignupComponent {}
 
 const routes: Routes = [
-  { path: '', component: LoginComponent }
+  { path: 'auth/signin', component: SignupComponent },
+  { path: 'auth/signup', component: SignupComponent }
 ];
 
 describe('SigninComponent', () => {
+
   let component: SigninComponent;
   let fixture: ComponentFixture<SigninComponent>;
   let nativeElement: HTMLElement;
+  let pageTitle: HTMLElement;
+  let button: HTMLButtonElement;
+  let anchor: HTMLElement;
+  let emailInput: HTMLInputElement;
+  let passwordInput: HTMLInputElement;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
         SigninComponent,
-        LoginComponent
+        SignupComponent
       ],
       imports: [
         BrowserAnimationsModule,
         HttpClientModule,
         SharedModule,
-        RouterModule.forRoot(routes)
+        RouterTestingModule.withRoutes(routes)
       ],
     })
     .compileComponents();
   }));
 
-  beforeEach(() => {
+  beforeEach(async(() => {
     fixture = TestBed.createComponent(SigninComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
     nativeElement = fixture.nativeElement;
+    pageTitle = nativeElement.querySelector('h2');
+    emailInput = nativeElement.querySelector('input[type="email"');
+    passwordInput = nativeElement.querySelector('input[type="password"');
+    button = nativeElement.querySelector('button[type="button"');
+    anchor = nativeElement.querySelector('p small a');
+  }));
+
+  afterEach(() => {
+    fixture = null;
+    component = null;
+    nativeElement = null;
+    pageTitle = null;
+    emailInput = null;
+    passwordInput = null;
+    button = null;
+    anchor = null;
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
-  it('should have email input', () => {
-    const emailInput = nativeElement.querySelector('input[type="email"');
+  it('should have correct page title', () => {
+    expect(pageTitle.innerText).toBe('Signin');
+  });
 
-    // expect(emailInput).toBeTruthy();
+  it('should have email input', () => {
+    expect(emailInput).toBeTruthy();
+    expect(emailInput.placeholder).toBe('email');
+  });
+
+  it('should have password input', () => {
+    expect(passwordInput).toBeTruthy();
+    expect(passwordInput.placeholder).toBe('password');
+  });
+
+  it('should have submit button', () => {
+    expect(button).toBeTruthy();
+    expect(button.innerText).toBe('signin');
+  });
+
+  it('should have link to signup', () => {
+    expect(anchor).toBeTruthy();
+    expect(anchor.innerText).toBe('signup');
+    expect(anchor.getAttribute('routerLink')).toBe('/auth/signup');
+  });
+
+  it('should navigate to signup page', () => {
+    inject([ Router, Location ], fakeAsync((router: Router, location: Location) => {
+      router.initialNavigation();
+      tick();
+      expect(location.path()).toBe('/auth/signin');
+      anchor.dispatchEvent(new Event('click'));
+      tick();
+      expect(location.path()).toBe('/auth/signup');
+    }))
   });
 });
